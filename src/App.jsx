1104a14e47158f5831bb3ba3dbbe55a87730968b
PoshikaM <145483042+PoshikaM@users.noreply.github.com>
2024-01-29 22:34:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import KalviumNotes from './Components/Editor';
+
+function App() {
+  return (
+    <div className="App">
+      <KalviumNotes/>
+    </div>
+  );
+}
+
+export default App;
